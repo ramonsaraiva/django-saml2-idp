11591f65d9a59b123175fb7a56ba29073fb60f67
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-09-17 14:43:08 -0300

    Add sample index (login required) and login pages

--- saml/settings.py
+++ saml/settings.py
@@ -33,7 +33,7 @@ ROOT_URLCONF = 'saml.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- saml/urls.py
+++ saml/urls.py
@@ -3,8 +3,13 @@ from django.conf.urls import (
     url,
 )
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^idp/', include('djangosaml2idp.urls')),
+    url(r'^$', login_required(TemplateView.as_view(template_name="index.html"))),
 ]
