767f89b31c0a63c6d11c925674f258727fd3bca9
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-09-18 11:21:23 -0300

    Add a protected resource request to the SP example

--- saml/urls.py
+++ saml/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^idp/', include('djangosaml2idp.urls')),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^iframe/', TemplateView.as_view(template_name="iframe.html")),
 ]
